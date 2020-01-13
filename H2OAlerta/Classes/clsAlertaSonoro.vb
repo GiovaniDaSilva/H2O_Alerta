441e846f54c6829454058a71d0b71395ddbd677b
Giovani da Silva <giovani-vani@hotmail.com>
2020-01-13 13:05:16 -0300

    Add files via upload
    Adicionado audio dentro do .exe

--- H2OAlerta/Classes/clsAlertaSonoro.vb
+++ H2OAlerta/Classes/clsAlertaSonoro.vb
@@ -1,7 +1,7 @@
 ﻿Public Class clsAlertaSonoro
 
     Public Shared sub subExecutaAlerta()
-        My.Computer.Audio.Play(Application.StartupPath  & "\Agua.wav", AudioPlayMode.Background)                 
+        My.Computer.Audio.Play(My.Resources.Agua , AudioPlayMode.Background)             
     End sub
 
 End Class
